f3d10490f206b1e63060a0a92bf0af65e7c60942
Jamie Wu <jvyu59jvyu@icloud.com>
2021-12-16 10:03:09 +0800

    chore: bundle cocoapods install

---
+++ ios/Pods/Target Support Files/Pods-RNFastlanePratice-RNFastlanePraticeTests/Pods-RNFastlanePratice-RNFastlanePraticeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNFastlanePratice_RNFastlanePraticeTests : NSObject
+@end
+@implementation PodsDummy_Pods_RNFastlanePratice_RNFastlanePraticeTests
+@end
